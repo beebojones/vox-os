2ebec44e00709671be304c5bf93f9bdbfb1acb4b
beebojones <karabaw@prodigy.net>
2026-01-07 21:21:18 -0600

    Update VoxDashboard.jsx

--- frontend/src/pages/VoxDashboard.jsx
+++ frontend/src/pages/VoxDashboard.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { toast } from "sonner";
-import TasksPanel from "@/components/TasksPanel";
+import TasksPanel from "/components/TasksPanel";
 
 import {
   Send,
@@ -339,3 +339,4 @@ export default function VoxDashboard() {
     </div>
   );
 }
+
